d3242e4f94b3bd22cf6a7c1d518efa5b25ffa859
Joni Laukkanen <joni.laukkanen@metropolia.fi>
2021-07-26 14:42:08 +0300

    Osa1 kurssitiedot valmis

--- osa 1/kurssitiedot/src/App.js
+++ osa 1/kurssitiedot/src/App.js
@@ -36,6 +36,22 @@ const Total = (props) => {
   );
 };
 
+const Hello = (props) => {
+  const bornYear = () => {
+    const yearNow = new Date().getFullYear();
+    return yearNow - props.age;
+  };
+
+  return (
+    <div>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
+      <p>So you were probably born {bornYear()}</p>
+    </div>
+  );
+};
+
 const App = () => {
   const course = {
     name: "Half Stack application development",
